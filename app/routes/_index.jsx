5f4eab806b6a04b9da85f31bb966f565eef5d3ff
Matthijs Boet <m.boet@chello.nl>
2023-04-19 14:03:17 +0200

    init commit

---
+++ app/routes/_index.jsx
@@ -0,0 +1,17 @@
+import { Link } from "@remix-run/react";
+
+export const meta = () => {
+  return [{ title: "New Remix App" }];
+};
+
+export default function Index() {
+  return (
+    <main id="content">
+      <h1>A better way of keeping track of your notes</h1>
+      <p>Try our early beta and never loose track of your notes again!</p>
+      <p id="cta">
+        <Link to="/notes">Try Now!</Link>
+      </p>
+    </main>
+  );
+}
